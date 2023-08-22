a5e8c2cd0da2c6267e60f80b4824ca8a102f44b2
Pradumn Srivastava <131151467+PradumnSrivastava@users.noreply.github.com>
2023-08-22 23:41:26 +0530

    Add files via upload

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,49 @@
+// LeetCode Problem No.33
+
+// Search in Rotated Sorted Array
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int start = 0;
+        int end = n - 1;
+        int mid = start + (end - start) / 2;
+
+        while (start <= end)
+        {
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[start])
+            {
+                if (target >= nums[start] && target <= nums[mid])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            else
+            {
+                if (target > nums[mid] && target <= nums[end])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            mid = start + (end - start) / 2;
+        }
+        return -1;
+    }
+};
